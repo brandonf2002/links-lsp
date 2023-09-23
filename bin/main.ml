9a393b276d2928bee75e18530fb306f6fdf47e4f
Brandon Forrest <brandonf2002@live.co.uk>
2023-09-23 21:21:22 +0100

    Can parse the headers now

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,5 @@
+(* The communication_channel stuff *)
+
 type file = string
 type tcp_stream = int
 
@@ -6,11 +8,53 @@ type communication_channel =
   | Pipe of file
   | Socket of tcp_stream
 
-let describe_channel ch =
-  match ch with
+let describe_channel (ch : communication_channel) =
+    match ch with
   | Stdio -> "Standard I/O"
   | Pipe f -> "Pipe with file: " ^ f  (* Assuming f can be converted to string *)
   | Socket s -> "Socket with stream: " ^ (string_of_int s)  (* Assuming s can be converted to string *)
+
+type header_info = {
+  content_length : int;
+  content_type : string option;
+}
+
+exception InvalidHeader
+
+let read_line () =
+  try Some (input_line stdin) with End_of_file -> None
+
+let parse_header () : header_info =
+  let rec aux length ctype = 
+    match read_line () with
+    | Some "" -> { content_length = length; content_type = ctype }  (* Empty line indicates end of headers *)
+    | Some line ->
+      (match String.split_on_char ':' line with
+       | "Content-Length" :: value :: [] -> aux (int_of_string (String.trim value)) ctype
+       | "Content-Type" :: value :: [] -> aux length (Some (String.trim value))
+       | _ -> aux length ctype)
+    | None -> raise InvalidHeader
+  in aux 0 None
+
+let read_message_stdio () : string =
+  let header = parse_header () in
+  let buffer = Bytes.create header.content_length in
+  really_input stdin buffer 0 header.content_length;
+  Bytes.to_string buffer
+
+let read_message ch : string =
+  match ch with
+  | Stdio -> read_message_stdio ()
+  | Pipe f -> failwith "Not implemented"
+  | Socket s -> failwith "Not implemented"
+
+let write_message ch : string =
+  match ch with
+  | Stdio -> failwith "Not implemented"
+  | Pipe f -> failwith "Not implemented"
+  | Socket s -> failwith "Not implemented"
+
+(* The Argument Parsing *) 
 
 let method_ref = ref Stdio (* Default communication method *)
 let option_counter = ref 0 (* Number of communication method options specified (Fail if more than 1) *)
@@ -32,6 +76,8 @@ let specs =
     "--clientProcessId", Arg.String (fun s -> client_pid_ref := Some s), " Store the client's process ID"
   ]
 
+(* Main *)
+
 let () =
   Arg.parse specs (fun _ -> ()) "Usage: links_lsp [options]";
   
@@ -41,8 +87,7 @@ let () =
 
   describe_channel !method_ref |> print_endline;
 
-  (* match !method_ref with *)
-  (* | Stdio -> print_endline "Using stdio" *)
-  (* | (Pipe s) -> Printf.printf "Using named pipes: %s\n" s *)
-  (* | (Socket i) -> Printf.printf "Using TCP/IP socket on port: %d\n" i *)
-  (* | None -> print_endline "Using stdio (default)" *)
+  match !method_ref with
+  | Stdio -> read_message_stdio () |> print_endline
+  | (Pipe s) -> failwith "Not implemented"
+  | (Socket i) -> failwith "Not implemented"
