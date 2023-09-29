69ec3e8b55fa77679c8565b1c780ecfb3992d0ad
Brandon Forrest <brandonf2002@live.co.uk>
2023-09-29 18:20:15 +0100

    Finalised the move of components

--- lib/cli_args.mli
+++
@@ -1,6 +0,0 @@
-(* Expose the parsing function and refs that main will use *)
-open CommunicationChannel
-
-val parse_args : unit -> unit
-val method_ref : CommunicationChannel.t ref
-val client_pid_ref : string option ref
