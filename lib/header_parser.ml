6f8051c1f8ea9ed3488164ded205c978b95577bf
Brandon Forrest <brandonf2002@live.co.uk>
2023-10-02 16:14:28 +0100

    Just some small formatting

--- lib/header_parser.ml
+++ lib/header_parser.ml
@@ -3,16 +3,6 @@ type header_info = {
   content_type : string option;
 }
 
-(* let write_to_file content file_name = *)
-(*   let oc = open_out file_name in *)
-(*   output_string oc content; *)
-(*   close_out oc *)
-
-(* let append_to_file content file_name = *)
-(*   let oc = open_out_gen [Open_creat; Open_text; Open_append] 0o666 file_name in *)
-(*   output_string oc (content ^ "\n"); *)
-(*   close_out oc *)
-
 exception InvalidHeader
 
 let read_line () =
@@ -22,10 +12,8 @@ let parse_header () : header_info =
   let rec aux length ctype = 
     match read_line () with
     | Some line -> (match String.trim line with
-      | "" -> 
-          { content_length = length; content_type = ctype }  (* Empty line indicates end of headers *)
-      | line ->
-        (match String.split_on_char ':' line with
+      | "" -> { content_length = length; content_type = ctype }
+      | line -> (match String.split_on_char ':' line with
          | "Content-Length" :: value :: [] -> aux (int_of_string (String.trim value)) ctype
          | "Content-Type" :: value :: [] -> aux length (Some (String.trim value))
          | _ -> aux length ctype));
