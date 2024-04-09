1f022c486c38fb6b94db1083880da6edccf35162
Brandon Forrest <br@ndon.me>
2024-04-09 10:00:26 +0100

    Columns do not work

--- lib/links_ls/diagnostics.ml
+++ lib/links_ls/diagnostics.ml
@@ -1,3 +1,7 @@
+(* TEMP
+   TODO: Move to separate file *)
+(* END TEMP *)
+
 open Common
 open Links_lsp.Common
 
@@ -11,6 +15,36 @@ module ItemTable = Hashtbl.Make (struct
 let diagnostics = ItemTable.create 10
 let clear_diagnostics uri = ItemTable.remove diagnostics (Lsp.Uri.to_string uri)
 
+let extract_string_and_number (s : string) : string * int =
+  let prefix1 = "<string>:" in
+  let prefix2 = "***: Parse error: <string>:" in
+  if String.starts_with ~prefix:prefix1 s
+  then (
+    let num_start = String.length prefix1 in
+    let num_end = String.index_from s num_start ':' in
+    let str_start = num_end + 2 in
+    let num_str = String.sub s num_start (num_end - num_start) in
+    let num = int_of_string num_str in
+    let modified_str = String.sub s str_start (String.length s - str_start) in
+    modified_str, num)
+  else if String.starts_with ~prefix:prefix2 s
+  then (
+    let modified_str =
+      String.sub s (String.length prefix2) (String.length s - String.length prefix2)
+    in
+    let num_start = 0 in
+    let num_end = String.index_from modified_str num_start ' ' in
+    let num_str = String.sub modified_str num_start (num_end - num_start) in
+    let num = int_of_string (String.trim num_str) in
+    (* remove the number from the string *)
+    let modified_str =
+      String.sub modified_str (num_end + 1) (String.length modified_str - num_end - 1)
+    in
+    let modified_str = "Parse error: " ^ String.trim modified_str in
+    modified_str, num)
+  else s, 0
+;;
+
 let add_diagnostic uri error =
   let error_string = Links_core.Errors.format_exception error in
   let uri_string = Lsp.Uri.to_string uri in
@@ -19,10 +53,12 @@ let add_diagnostic uri error =
     | Some d -> d
     | None -> []
   in
+  log_to_file error_string;
+  let error_string, line_number = extract_string_and_number error_string in
   let range =
     Lsp.Types.Range.create
-      ~start:(Lsp.Types.Position.create ~line:0 ~character:0)
-      ~end_:(Lsp.Types.Position.create ~line:0 ~character:3)
+      ~start:(Lsp.Types.Position.create ~line:(line_number - 1) ~character:0)
+      ~end_:(Lsp.Types.Position.create ~line:(line_number - 1) ~character:3)
   in
   let new_d = Lsp.Types.Diagnostic.create ~message:error_string ~range () in
   ItemTable.replace diagnostics uri_string (new_d :: d_list)
